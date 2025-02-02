5c397b297cf48fd7bf1f3ca7cc909f8bceb57db5
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2025-02-02 16:56:13 +0100

    more content added

--- aperol/src/components/More.js
+++ aperol/src/components/More.js
@@ -7,9 +7,10 @@ class More extends Component {
   render() {
     return (
       <div className='more-section'>
-        <h1 className="more-title">More about Aperol history</h1>
+<h1 className="more-title">More about Aperol</h1>
         <div className="more-articles">
           <article className="more-article">
+<h1 className="more-article-title">Aperol history</h1>
 <p className="more-article-text1">
   Aperol is a brand of bitter orange liqueur, which was created in 1919 by the Italian company Campari.<br></br> 
   The name Aperol is derived from the Italian words <strong>"aperire"</strong> (to open) and <strong>"oro"</strong> (gold), 
@@ -41,12 +42,17 @@ class More extends Component {
               alt="Aperol Spritz" 
               className="more-article-image" 
             />
-            <p>In recent years, the Aperol Spritz has exploded in popularity beyond Italy. From rooftop bars in New York to beach clubs in Bali, the drink has become synonymous with summer and relaxation. Social media has played a significant role in its rise, with millions of posts showcasing the drink's photogenic appeal. The hashtag #AperolSpritz has become a global phenomenon, with influencers and everyday drinkers alike sharing their love for the cocktail.</p>
+            <p>In recent years, the Aperol Spritz has exploded in popularity beyond Italy. From rooftop bars in New York to beach clubs in Bali, the drink has become synonymous with summer and relaxation. Social media has played a significant role in its rise, with millions of posts showcasing the drink's photogenic appeal. The hashtag<strong> #AperolSpritz</strong> has become a global phenomenon, with influencers and everyday drinkers alike sharing their love for the cocktail.</p>
           </article>
 
           <article className="more-article">
-            <h2>Modern Culture</h2>
-            <p>Today, Aperol Spritz has become synonymous with Italian aperitivo culture and is enjoyed worldwide as a refreshing summer cocktail.</p>
+        <h2>Modern Culture</h2>
+        <img 
+          src={`${process.env.PUBLIC_URL}/gallery-images/happy.jpg`}
+              alt="Aperol Spritz" 
+              className="more-article-image" 
+            />
+<p>The Aperol Spritz is more than just a trend—it’s a reflection of modern culture’s desire for beauty, connection, and balance. It encapsulates the joy of living in the moment, the importance of social bonds, and the celebration of individuality. As we navigate the complexities of the 21st century, the Aperol Spritz reminds us to pause, savor, and enjoy the vibrant flavors of life. Salute! 🍊.</p>
           </article>
         </div>
       </div>
